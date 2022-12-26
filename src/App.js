3a55eced98822e9cbbfed36b42ae5295daf1f503
picuti256 <picuti256@gmail.com>
2022-12-26 12:34:32 -0300

    Parte visual da calculadora realziada. Inicio das funções

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import { Container, Content } from "./styles";
 import Input from "./components/Input";
-// import Button from "./components/Button";
+import Button from "./components/Button";
+
+import { Container, Content, Row } from "./styles";
+import { useState } from "react";
+
+
 
 const App = () => {
+	const [currentNumber, setCurrentNumber] = useState('0');
+
+	const handleAddNumber = (number) => {
+		setCurrentNumber(prev => `${number}${prev}`)
+	}
 	return (
 		<Container>
 			<Content>
-				<Input />
+				<Input value={currentNumber} />
+				<Row>
+					<Button label="7" />
+					<Button label="8" />
+					<Button label="9" />
+					<Button label="-" />
+				</Row>
+				<Row>
+					<Button label="5" />
+					<Button label="5" />
+					<Button label="6" />
+					<Button label="+" />
+				</Row>
+				<Row>
+					<Button label="1" />
+					<Button label="2" />
+					<Button label="3" />
+					<Button label="=" onClick={() => handleAddNumber('')} />
+				</Row>
+				<Row>
+					<Button label="0" />
+					<Button label="/" />
+					<Button label="C" />
+					<Button label="X" />
+				</Row>
 			</Content>
 		</Container>
 	);

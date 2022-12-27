4c0cfb7d583ef73d7349687232612ddae93100c4
picuti256 <picuti256@gmail.com>
2022-12-27 09:48:27 -0300

    Criação da função de soma e igual

--- src/App.js
+++ src/App.js
@@ -8,9 +8,39 @@ import { useState } from "react";
 
 const App = () => {
 	const [currentNumber, setCurrentNumber] = useState('0');
+	const [firstNumber, setFirstNumber] = useState('0');
+	const [operation, setOperation] = useState('');
 
+	const handleOnClear = () => {
+		setCurrentNumber('0')
+		setFirstNumber('0')
+	}
 	const handleAddNumber = (number) => {
-		setCurrentNumber(prev => `${number}${prev}`)
+		setCurrentNumber(prev => `${prev === '0' ? '' : prev}${number}`)
+	}
+
+	const handleSumNumber = () => {
+		if (firstNumber === '0') {
+			setFirstNumber(String(currentNumber));
+			setCurrentNumber('0')
+			setOperation('+')
+		} else {
+			const sum = Number(firstNumber) + Number(currentNumber)
+			setCurrentNumber(String(sum));
+			setOperation('')
+		}
+	}
+
+	const handleEquals = () => {
+		if (firstNumber !== '0' && operation !== '' && currentNumber !== '0') {
+			switch (operation) {
+				case '+':
+					handleSumNumber();
+					break;
+				default:
+					break;
+			}
+		}
 	}
 	return (
 		<Container>
@@ -26,18 +56,18 @@ const App = () => {
 					<Button label="5" onClick={() => handleAddNumber('4')} />
 					<Button label="5" onClick={() => handleAddNumber('5')} />
 					<Button label="6" onClick={() => handleAddNumber('6')} />
-					<Button label="+" onClick={() => handleAddNumber('')} />
+					<Button label="+" onClick={handleSumNumber} />
 				</Row>
 				<Row>
 					<Button label="1" onClick={() => handleAddNumber('1')} />
 					<Button label="2" onClick={() => handleAddNumber('2')} />
 					<Button label="3" onClick={() => handleAddNumber('3')} />
-					<Button label="=" onClick={() => handleAddNumber('')} />
+					<Button label="=" onClick={handleEquals} />
 				</Row>
 				<Row>
 					<Button label="0" onClick={() => handleAddNumber('0')} />
 					<Button label="/" />
-					<Button label="C" />
+					<Button label="C" onClick={handleOnClear} />
 					<Button label="X" />
 				</Row>
 			</Content>

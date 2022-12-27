bb4f72ed09bda9e84a91ce127c05ea6acae77ee9
picuti256 <picuti256@gmail.com>
2022-12-27 08:44:05 -0300

    Ajustes de funções

--- src/App.js
+++ src/App.js
@@ -17,25 +17,25 @@ const App = () => {
 			<Content>
 				<Input value={currentNumber} />
 				<Row>
-					<Button label="7" />
-					<Button label="8" />
-					<Button label="9" />
-					<Button label="-" />
+					<Button label="7" onClick={() => handleAddNumber('7')} />
+					<Button label="8" onClick={() => handleAddNumber('8')} />
+					<Button label="9" onClick={() => handleAddNumber('9')} />
+					<Button label="-" onClick={() => handleAddNumber('')} />
 				</Row>
 				<Row>
-					<Button label="5" />
-					<Button label="5" />
-					<Button label="6" />
-					<Button label="+" />
+					<Button label="5" onClick={() => handleAddNumber('4')} />
+					<Button label="5" onClick={() => handleAddNumber('5')} />
+					<Button label="6" onClick={() => handleAddNumber('6')} />
+					<Button label="+" onClick={() => handleAddNumber('')} />
 				</Row>
 				<Row>
-					<Button label="1" />
-					<Button label="2" />
-					<Button label="3" />
+					<Button label="1" onClick={() => handleAddNumber('1')} />
+					<Button label="2" onClick={() => handleAddNumber('2')} />
+					<Button label="3" onClick={() => handleAddNumber('3')} />
 					<Button label="=" onClick={() => handleAddNumber('')} />
 				</Row>
 				<Row>
-					<Button label="0" />
+					<Button label="0" onClick={() => handleAddNumber('0')} />
 					<Button label="/" />
 					<Button label="C" />
 					<Button label="X" />
